d58e367259aaf9cae4529421d5915b6cfd2afb4d
golnar <g.mordadi@aufnet.com>
2017-07-25 18:04:26 +0430

    MerchantProfileDto & MerchantProfileShared

---
+++ Backend/Aufnet.Backend.Api/Controllers/MerchantProfileControlle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Aufnet.Backend.ApiServiceShared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aufnet.Backend.Api.Controllers
+{
+    [Route("api/merchants/{username}/profile")]
+    public class MerchantProfileControlle : BaseController
+    {
+
+        // GET api/merchants/golnar/merchantprofile
+        
+        [HttpGet]
+        public IEnumerable<string> Get(string username)
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/merchants/golnar/merchantprofile
+        [HttpPost]
+        public void Post([FromBody]CustomerProfileDto value)
+        {
+        }
+
+        // PUT api/merchants/golnar/merchantprofile
+        [HttpPut]
+        public void Put(string username, [FromBody]CustomerProfileDto value)
+        {
+        }
+    }
+}
---
+++ Backend/Aufnet.Backend.ApiServiceShared/Aufnet.Backend.ApiServiceShared/Models/MerchantProfileDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Aufnet.Backend.Data.Models.Entities.Shared;
+
+namespace Aufnet.Backend.ApiServiceShared.Models
+{
+    public class MerchantProfileDto
+    {
+        public string BusinessName { get; set; }
+        public virtual Address Address { get; set; }
+    }
+}
