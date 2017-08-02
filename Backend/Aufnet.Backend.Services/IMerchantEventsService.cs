d6f61e5a5b7bdb7e6ab5a55e8427fd1f32e437c5
golnar <g.mordadi@aufnet.com>
2017-08-02 16:28:20 +0430

    EditEvent Service

--- Backend/Aufnet.Backend.Services/IMerchantEventsService.cs
+++ Backend/Aufnet.Backend.Services/IMerchantEventsService.cs
@@ -1,14 +1,11 @@
 using System.Threading.Tasks;
-using Aufnet.Backend.ApiServiceShared.Models;
 using Aufnet.Backend.ApiServiceShared.Models.Merchant;
-using Aufnet.Backend.ApiServiceShared.Models.Shared;
 using Aufnet.Backend.ApiServiceShared.Shared;
 
 namespace Aufnet.Backend.Services
 {
     public interface IMerchantEventsService
     {
-
         Task<IGetServiceResult<MerchantEventsDto>> GetEvents(string username);
         Task<IServiceResult> AddEvent(string username, MerchantEventsDto value);
         Task<IServiceResult> UpdateEvent(string username, MerchantEventsDto value);
